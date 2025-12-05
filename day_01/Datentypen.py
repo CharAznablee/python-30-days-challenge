69133efdbecb8c2afca37f17e99faf6a349ccdef
CharAznablee <rahat190600@gmail.com>
2025-12-05 22:45:41 +0100

    Day01 + Datentypen hinzugefügt

---
+++ day_01/Datentypen.py
@@ -0,0 +1,42 @@
+"Zusatz zur Aufgabe 1: Alle Datentypen"
+
+# INT ist für Ganze Zahlen
+zahl_int = 20
+
+# STRING ist für eine Text zeile der text muss in "" eingeschrieben werden!
+text_string = "Hallo ich bin ein String"
+
+# FLOAT ist für Gleitkommazahlen
+zahl_float = 1.25
+
+# List für Geordnete listen und Veränderbare Sammlungen von Elementen
+liste = [1, 'apfel', 2, 3]
+
+# tuple Geordnet aber unveränderbare Sammlungen von elementen
+un_list = ("Affe", "Bär", "Löwe")
+
+#range eine unverändliche Sequenz von Zahlen
+reichweite = range(1, 5)
+
+#BOOL, Wahrheitswerte entweder True oder False
+tr_ue = True
+fa_lse = False
+
+#dict Ungeordnete oder Geordnete sammlungen von Schlüssel-wert-paaren 
+dictionary = {'Name': 'Hans', 'Alter': 30}
+
+#set: Eine ungeordnete Sammlung von eindeutigen Elementen
+thisset = {"apple", "banana", "pineapple"}
+
+#Jeder print befehl gibt das jeweilige Datentyp auf der Konsole aus
+print(zahl_int)
+print(text_string)
+print(zahl_float)
+print(liste)
+print(un_list)
+print(reichweite)
+print(tr_ue)
+print(fa_lse)
+print(dictionary)
+print(thisset)
+
--- day_01/log.md
+++ day_01/log.md
@@ -1,11 +1,16 @@
 # Day X - Log
 **Was habe ich gelernt?**
+- Wie man Variablen nutzt
+- Was genau welche Datentypen sind
+- Wie ich die variablen ausgebe in der Konsole
 
 **Was war schwer?**
+- Sich an die Datentypen zu erinnern war relativ schwer
 
 **Was hat mir besonders geholfen**
-
+- Ich hab mich an frühere aufgaben erinnert was mir beim lösen der aufgabe geholfen hat
 **Wie ist mein Gefühl heute?**
-
+- Ich denke ich könnte noch eine weitere daily challenge machen aber ich belasse es dabei
 **GPT genutzt?**
    Nein / Ja (und warum)
+- Nein ich hab GPT nicht genutzt, aber ich hab im internet nachgeschaut welche datentypen es gibt, und wie die unterschiedlichen datentypen heißen
