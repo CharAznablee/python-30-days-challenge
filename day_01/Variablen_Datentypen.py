59803a7e1903f07a1db7f95022c7ad124f89e65f
CharAznablee <rahat190600@gmail.com>
2025-12-05 22:29:34 +0100

    Update der Day01.py

--- day_01/Variablen_Datentypen.py
+++ day_01/Variablen_Datentypen.py
@@ -4,6 +4,8 @@
 name = "rahat"
 #Variable für den Alter erstellen mit dem datentyp INT
 alter = 25
+#weitere String Variable für die ausgabe des Ziels
+ziel = "Python kenntnisse zuvertiefen, und meine abhängigkeit zu GPT modellen loswerden"
 
 #Print befehl zum Anzeigen der beiden Variablen
-print("Mein Name ist:", name, "und ich bin", alter, "Jahre alt")
+print("Mein Name ist:", name, "und ich bin", alter, "Jahre alt. Mein Ziel mit der Challenge ist:", ziel)
