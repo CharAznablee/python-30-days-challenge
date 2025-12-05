cc9671ab00f449236da99ac4a4c8b5d841084970
CharAznablee <rahat190600@gmail.com>
2025-12-05 22:26:02 +0100

    Day 01 Completed: Basics

---
+++ day_01/Variablen_Datentypen.py
@@ -0,0 +1,9 @@
+"Aufgabe1: Variablen und Datentypen, Ziel der aufgabe verständnis für unterschiedliche datentypen erlangen"
+
+#Variable für den namen erstellen mit dem datentyp STRING
+name = "rahat"
+#Variable für den Alter erstellen mit dem datentyp INT
+alter = 25
+
+#Print befehl zum Anzeigen der beiden Variablen
+print("Mein Name ist:", name, "und ich bin", alter, "Jahre alt")
